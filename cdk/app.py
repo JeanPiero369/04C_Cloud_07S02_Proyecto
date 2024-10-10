0269408fa339d7f5b43c96eac1cbeb17ac71ec08
unknown <jean.huaylla@utec.edu.pe>
2024-10-10 15:29:28 -0500

    cambios

--- cdk/app.py
+++ cdk/app.py
@@ -7,7 +7,6 @@ from cdk.cdk_stack import CdkStack
 
 app = cdk.App()
 CdkStack(app, "CdkStack",
-    env=Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region="us-east-1")
-    )
+    env=Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region="us-east-1"))
 
 app.synth()
