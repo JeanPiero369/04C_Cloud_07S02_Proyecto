149ecf883c5d26e81151dfdb4fff2a40f69779a0
unknown <jean.huaylla@utec.edu.pe>
2024-10-10 15:34:29 -0500

    cambios

--- cdk/app.py
+++ cdk/app.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 import os
 
-import aws_cdk as cdk,Environment
+from aws_cdk import App, Environment
 
 from cdk.cdk_stack import CdkStack
 
-app = cdk.App()
+app = App()
 CdkStack(app, "CdkStack",
-    env=Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region="us-east-1"))
+    env=Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region="us-east-1")
+)
 
 app.synth()
