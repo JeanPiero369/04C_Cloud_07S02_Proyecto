61e0a79339520ec33663d649addea8c4a3ef42c5
unknown <jean.huaylla@utec.edu.pe>
2024-10-10 16:48:47 -0500

    cambios

--- cdk/cdk/cdk_stack.py
+++ cdk/cdk/cdk_stack.py
@@ -3,7 +3,8 @@ from aws_cdk import (
     aws_ec2 as ec2,
     aws_iam as iam,
     CfnOutput,
-    CfnParameter
+    CfnParameter,
+    aws_elasticloadbalancingv2 as elbv2,
 )
 from constructs import Construct
 
@@ -25,14 +26,31 @@ class CdkStack(Stack):
         # Security Group
         vpc = ec2.Vpc.from_lookup(self, "VPC", is_default=True)
 
-        security_group = ec2.SecurityGroup(self, "InstanceSecurityGroup",
+        security_group_produccion = ec2.SecurityGroup(self, "InstanceSecurityGroup",
             vpc=vpc,
+            security_group_name="Proyecto - GS Produccion"
             description="Permitir trafico SSH y HTTP desde cualquier lugar",
             allow_all_outbound=True
         )
 
-        security_group.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(22), "Allow SSH")
-        security_group.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(80), "Allow HTTP")
+        security_group_produccion.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(22), "Allow SSH")
+        security_group_produccion.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(80), "Allow HTTP")
+        security_group_produccion.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(8000), "Allow Traffic on Port 8000")
+        security_group_produccion.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(8001), "Allow Traffic on Port 8001")
+        security_group_produccion.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(8002), "Allow Traffic on Port 8002")
+        security_group_produccion.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(8003), "Allow Traffic on Port 8003")
+        
+        security_group_bd_datos = ec2.SecurityGroup(self, "InstanceSecurityGroup",
+            vpc=vpc,
+            security_group_name="Proyecto - GS Base de datos"
+            description="Permitir trafico SSH y HTTP desde cualquier lugar",
+            allow_all_outbound=True
+        )
+
+        security_group_bd_datos.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(22), "Allow SSH")
+        security_group_bd_datos.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(8000), "Allow Traffic on Port 8000")
+        security_group_bd_datos.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(8001), "Allow Traffic on Port 8001")
+        security_group_bd_datos.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(8002), "Allow Traffic on Port 8002")
 
         # Updated IAM Role ARN with new Account ID
         existing_role_arn = f"arn:aws:iam::{account_id}:role/LabRole"
@@ -44,7 +62,7 @@ class CdkStack(Stack):
             instance_name="Proyecto - MV Base de datos",
             instance_type=ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
             machine_image=ec2.MachineImage.generic_linux({"us-east-1": ami_id.value_as_string}),
-            security_group=security_group,
+            security_group=security_group_bd_datos,
             key_name="vockey",
             block_devices=[ec2.BlockDevice(
                 device_name="/dev/sda1",
@@ -57,8 +75,8 @@ class CdkStack(Stack):
             vpc=vpc,
             instance_name="Proyecto - MV Produccion 01",
             instance_type=ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
-            machine_image=ec2.MachineImage.generic_linux({"us-east-1": ami_id.value_as_string}),
-            security_group=security_group,
+            machine_image=ec2.MachineImage.generic_linux({"us-east-1a": ami_id.value_as_string}),
+            security_group=security_group_produccion,
             key_name="vockey",
             block_devices=[ec2.BlockDevice(
                 device_name="/dev/sda1",
@@ -71,8 +89,8 @@ class CdkStack(Stack):
             vpc=vpc,
             instance_name="Proyecto - MV Produccion 02",
             instance_type=ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
-            machine_image=ec2.MachineImage.generic_linux({"us-east-1": ami_id.value_as_string}),
-            security_group=security_group,
+            machine_image=ec2.MachineImage.generic_linux({"us-east-1b": ami_id.value_as_string}),
+            security_group=security_group_produccion,
             key_name="vockey",
             block_devices=[ec2.BlockDevice(
                 device_name="/dev/sda1",
@@ -86,7 +104,7 @@ class CdkStack(Stack):
             instance_name="Proyecto - MV Ingesta",
             instance_type=ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
             machine_image=ec2.MachineImage.generic_linux({"us-east-1": ami_id.value_as_string}),
-            security_group=security_group,
+            security_group=security_group_bd_datos,
             key_name="vockey",
             block_devices=[ec2.BlockDevice(
                 device_name="/dev/sda1",
@@ -94,6 +112,79 @@ class CdkStack(Stack):
             )],
             role=role
         )
+
+        # Crear un Target Group para las instancias de Producción
+        target_group_produccion_8000 = elbv2.ApplicationTargetGroup(self, "TargetGroup",
+            vpc=vpc,
+            target_type=elbv2.TargetType.INSTANCE,
+            health_check=elbv2.HealthCheck(
+                port="8000",
+                path="/",  # Ajusta esto según tu aplicación
+                interval=cdk.Duration.seconds(30)
+            ),
+            target_group_name="Proyecto - TG Produccion 8000"
+        )
+
+        target_group_produccion_8001 = elbv2.ApplicationTargetGroup(self, "TargetGroup",
+            vpc=vpc,
+            target_type=elbv2.TargetType.INSTANCE,
+            health_check=elbv2.HealthCheck(
+                port="8001",
+                path="/",  # Ajusta esto según tu aplicación
+                interval=cdk.Duration.seconds(30)
+            ),
+            target_group_name="Proyecto - TG Produccion 8001"
+        )
+
+        target_group_produccion_8002 = elbv2.ApplicationTargetGroup(self, "TargetGroup",
+            vpc=vpc,
+            target_type=elbv2.TargetType.INSTANCE,
+            health_check=elbv2.HealthCheck(
+                port="8002",
+                path="/",  # Ajusta esto según tu aplicación
+                interval=cdk.Duration.seconds(30)
+            ),
+            target_group_name="Proyecto - TG Produccion 8002"
+        )
+
+        target_group_produccion_8003 = elbv2.ApplicationTargetGroup(self, "TargetGroup",
+            vpc=vpc,
+            target_type=elbv2.TargetType.INSTANCE,
+            health_check=elbv2.HealthCheck(
+                port="8003",
+                path="/",  # Ajusta esto según tu aplicación
+                interval=cdk.Duration.seconds(30)
+            ),
+            target_group_name="Proyecto - TG Produccion 8003"
+        )
+
+        # Asociar instancias al Target Group
+        target_group_produccion_8000.add_targets("Targets", targets=[ec2_produccion_01, ec2_produccion_02])
+        target_group_produccion_8001.add_targets("Targets", targets=[ec2_produccion_01, ec2_produccion_02])
+        target_group_produccion_8002.add_targets("Targets", targets=[ec2_produccion_01, ec2_produccion_02])
+        target_group_produccion_8003.add_targets("Targets", targets=[ec2_produccion_01, ec2_produccion_02])
+
+
+        # Crear el Load Balancer
+        load_balancer = elbv2.ApplicationLoadBalancer(self, "LoadBalancer",
+            vpc=vpc,
+            internet_facing=True,
+            security_group=security_group_produccion,
+            load_balancer_name="Proyecto - LB Produccion" 
+        )
+
+        # Crear listeners para cada puerto
+        listener_8000 = load_balancer.add_listener("Listener8000", port=8000, open=True)
+        listener_8001 = load_balancer.add_listener("Listener8001", port=8001, open=True)
+        listener_8002 = load_balancer.add_listener("Listener8002", port=8002, open=True)
+        listener_8003 = load_balancer.add_listener("Listener8003", port=8003, open=True)
+
+        # Agregar los Target Groups a cada Listener
+        listener_8000.add_target_groups("TargetGroup8000",target_groups=[target_group_produccion_8000])
+        listener_8001.add_target_groups("TargetGroup8001",target_groups=[target_group_produccion_8001])
+        listener_8002.add_target_groups("TargetGroup8002",target_groups=[target_group_produccion_8002])
+        listener_8003.add_target_groups("TargetGroup8003",target_groups=[target_group_produccion_8003])
+
 
         ec2_bd_datos.add_user_data(
             "#!/bin/bash",
