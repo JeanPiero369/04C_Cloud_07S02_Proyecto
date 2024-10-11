ba311eacf2bce753ea09094317f4987fb00afca1
unknown <jean.huaylla@utec.edu.pe>
2024-10-10 19:35:09 -0500

    cambios

---
+++ Clientes/schemas.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+from typing import Optional
+
+# Esquema para Persona (base común entre Cliente y Agente)
+class Persona(BaseModel):
+    id_persona: Optional[int]=None # Asignado automáticamente por la base de datos
+    nombre: str
+    apellido: str
+    email: str
+    telefono: str
+
+# Esquema para Cliente, heredando de Persona
+class Cliente(Persona):
+    direccion: str
+    DNI: str
+
+# Esquema para Especialidad
+class Especialidad(BaseModel):
+    id_especialidad: Optional[int]=None # Asignado automáticamente por la base de datos
+    nombre_especialidad: str
+
+# Esquema para Agente, heredando de Persona
+class Agente(Persona):
+    direccion_oficina: str
+    id_especialidad: int  # Relación con la especialidad
